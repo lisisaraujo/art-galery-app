c7084a62cb7b67af2ec6fd06c953e00ab65f81dd
egbalwaldmann <egbal@waldmann.dev>
2023-02-21 12:12:10 +0100

    image problem resolved

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,20 @@ import Spotlight from "../Components/Spotlight";
 
 export default function HomePage({ pieces }) {
   const array = ["one", "two", "three"];
-
+  console.log(pieces);
   function randomArtwork(pieces) {
-    const number = pieces[Math.floor(Math.random() * pieces.length)];
-    const randomPiece = pieces[number];
+    const randomPiece = pieces[Math.floor(Math.random() * pieces.length)];
+
     return randomPiece;
   }
   const randomPiece = randomArtwork(pieces);
-// we need artist, name, imageSource
-  return <Spotlight name={randomPiece.name} artist={randomPiece.artist} imageSource={randomPiece.imageSource}/>;
+  console.log(randomPiece);
+  // we need artist, name, imageSource
+  return (
+    <Spotlight
+      name={randomPiece.name}
+      artist={randomPiece.artist}
+      imageSource={randomPiece.imageSource}
+    />
+  );
 }
