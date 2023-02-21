37ad30ac115c1e6d0f136804efae8319f0f188e5
nmarforio <n.marforio@gmail.com>
2023-02-21 11:20:03 +0100

    Error and Loading of fetcher

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import GlobalStyle from "../styles";
 import useSWR from "swr";
+import ArtPieces from "./pieces";
 
 export default function App({ Component, pageProps }) {
   const URL = "https://example-apis.vercel.app/api/art";
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { data, error, isLoading } = useSWR(URL, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
   const pieces = data;
   console.log(pieces);
 
