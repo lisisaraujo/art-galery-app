dae8d02a26b2f253ea8256f8d2c3d56cd166e1f4
yoricktf <yorick.tenfeld@gmail.com>
2023-02-22 17:50:41 +0100

    style changes

--- pages/pieces/[slug].js
+++ pages/pieces/[slug].js
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router'
 import Image from 'next/image';
 import CommentForm from "../../Components/CommentForm";
 import FavoriteButton from "../../Components/FavoriteButton";
+import styled from 'styled-components'
 
 export default function ShowPage({ pieces, artPiecesInfo, updateArtPiecesInfo, onToggleFavorite }) {
   const router = useRouter()
@@ -18,13 +19,14 @@ export default function ShowPage({ pieces, artPiecesInfo, updateArtPiecesInfo, o
     updateArtPiecesInfo(draft => {
       let currentPiece = draft.defaultValue.find(piece => piece.slug === slug)
       if (!currentPiece) {
-        return [
-          ...draft, {
+
+        draft.push(
+          {
             slug,
             isFavorite: false,
             comments: [commentAndDate],
           }
-        ]
+        )
       } else {
         currentPiece.comments = [commentAndDate, ...currentPiece.comments];
       }
@@ -38,14 +40,30 @@ export default function ShowPage({ pieces, artPiecesInfo, updateArtPiecesInfo, o
 
 
   return (
-    <>
-      <Image
-        src={imageSource}
-        alt="image of the day"
-        width={600}
-        height={600}
-      // fill
-      />
+    <Container>
+      <ImageContainer>
+        <Image
+          src={imageSource}
+          alt="image of the day"
+          width={600}
+          height={600}
+        // fill
+        />
+      </ImageContainer>
+      <ColorsContainer>
+        {colors.map((color) => {
+          return (
+            <>
+              <ColorDiv key={color} style={{ backgroundColor: color }}>
+                {color}
+              </ColorDiv>
+
+            </>
+
+          );
+        })}
+
+      </ColorsContainer>
       <section className="paintingDetails">
         <p>Name: {name}</p>
         <p>Artist: {artist}</p>
@@ -54,15 +72,42 @@ export default function ShowPage({ pieces, artPiecesInfo, updateArtPiecesInfo, o
       </section>
       <FavoriteButton slug={slug} onToggleFavorite={onToggleFavorite} />
       <CommentForm onSubmitComment={handleSubmitComment} />
-      {currentInfo && currentInfo.comments.map((comment, index) => {
-        return (
-          <div key={index}>
-            <p>{comment.comment}</p>
-            <p>{comment.date}</p>
-          </div>
-        )
-      })}
-
-    </>
+      {currentInfo &&
+        currentInfo.comments.map((comment, index) => {
+          return (
+            <div key={index}>
+              <p>{comment.comment}</p>
+              <p>{comment.date}</p>
+            </div>
+          );
+        })}
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ImageContainer = styled.div`
+  margin: 20px 0;
+`;
+
+const ColorsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 600px;
+
+`;
+
+const ColorDiv = styled.div`
+  background-color: #fff;
+  flex-grow: 1;
+  color: #000;
+  padding: 10px;
+  font-size: 0.7rem;
+  height: 30px;
+  /* color: rgb(60, 60, 60); */
+`;
