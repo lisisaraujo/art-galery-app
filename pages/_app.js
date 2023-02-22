6263d3dcd88cda321ac5d5554603a108d3c79258
nmarforio <n.marforio@gmail.com>
2023-02-22 14:15:24 +0100

    uselocalstorageImmer

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,16 @@ import { useEffect, useState } from "react";
 import { useImmer } from "use-immer";
 import ArtPieces from "./pieces";
 import Layout from "../Components/Layout";
+import { useImmerLocalStorageState } from "./useImmerLocalStorageState";
+
+// ...
 
 export default function App({ Component, pageProps }) {
-  const [artPiecesInfo, updateArtPiecesInfo] = useImmer([]);
+  const [artPiecesInfo, updateArtPiecesInfo] = useImmerLocalStorageState(
+    "artPiecesInfo",
+    { defaultValue: [] }
+  );
+  // const [artPiecesInfo, updateArtPiecesInfo] = useImmer([]);
 
   const URL = "https://example-apis.vercel.app/api/art";
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
@@ -19,15 +26,13 @@ export default function App({ Component, pageProps }) {
 
   function handleToggleFavorite(slug) {
     updateArtPiecesInfo((draft) => {
+      console.log(draft);
       const artPiece = draft.find((piece) => piece.slug === slug);
       if (!artPiece) {
-        return [
-          ...draft,
-          {
-            slug,
-            isFavorite: true,
-          },
-        ];
+        draft.push({
+          slug,
+          isFavorite: true,
+        });
       } else {
         artPiece.isFavorite = !artPiece.isFavorite;
       }
